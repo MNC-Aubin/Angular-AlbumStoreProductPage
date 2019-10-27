ee6bd98589f01dbf70735d4766fe15c35c59fd5e
MNC Aubin <mouafoaubin@gmail.com>
2019-10-27 03:36:08 +0100

    Completed module

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Product } from '../product';
 
-import { Productservicce, ProductService } from '../product.service';
+import { ProductService} from '../product.service';
 
 @Component({
   selector: 'app-product-list',
